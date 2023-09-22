64a156645a362dc430ad8e697ccbb617e8d3298b
Aegistahl <iant@jenixstudios.com>
2023-09-21 20:05:31 -0500

    Initial commit.

---
+++ Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RankingAppNET6.Models;
+
+namespace RankingAppNET6.Data
+{
+    public class DataContext : DbContext
+    {
+
+
+        public DataContext()
+        {
+        }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ItemModel> ItemModels => Set<ItemModel>();
+    }
+
+}
+
--- Models/ItemModel.cs
+++ Models/ItemModel.cs
@@ -5,7 +5,7 @@ namespace RankingAppNET6.Models
 	{
 		public int Id { get; set; }
 
-		public string Title { get; set; }
+		public string Title { get; set; } = string.Empty;
 
 		public int ImageId { get; set; }
 
